acd5f557897684f43217599cde5e85a61bce77bb
your_username <moha.kather@gmail.com>
2023-01-31 16:10:54 +0530

    added

---
+++ src/test/java/com/example/demo_springboot/DemoSpringbootApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.example.demo_springboot;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class DemoSpringbootApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
